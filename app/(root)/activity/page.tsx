1e4cad0c139a5db6c253b1f1cbd5e86df5905a45
Freddie Yang <dark_yty@hotmail.com>
2023-08-16 20:51:37 +1000

    added activity and webhook

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,5 +1,5 @@
 import ProfileHeader from "@/components/shared/ProfileHeader";
-import { fetchUser,  fetchUsers } from "@/lib/actions/user.actions";
+import { fetchUser, fetchUsers, getActivity } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs"
 
 import { redirect } from "next/navigation";
@@ -7,24 +7,56 @@ import { profileTabs } from "@/constants";
 import Image from "next/image";
 import ThreadsTab from "@/components/shared/ThreadsTab";
 import UserCard from "@/components/cards/UserCard";
+import Link from "next/link";
 
 // params is to get the id from the url
 async function Page() {
-    const user = await currentUser()
+  const user = await currentUser()
 
-    if (!user) return null;
+  if (!user) return null;
 
-    const userInfo = await fetchUser(user.id);
-    if (!userInfo?.onboarded) redirect("/onboarding")
-  
-    // Get activities 
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding")
+
+  // Get activities 
+  const activity = await getActivity(userInfo._id)
 
 
-    return (
-      <section>
-          <h1 className="head-text mb-10" >Activities</h1>
+  return (
+    <section>
+      <h1 className="head-text mb-10" >Activities</h1>
+
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
+          <>
+            {activity.map((activity) => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+
+                <article className="activity-card">
+                  <Image
+                    src={activity.author.image}
+                    alt="Profile Picture"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {activity.author.name}
+                    </span>{""}
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+
+        ) : <p className="!text-base-regular text-light-3">No activity yet.</p>}
+
       </section>
-    )
-  }
-  
-  export default Page
+    </section>
+  )
+}
+
+export default Page
